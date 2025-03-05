d62c2cf192651d28bc719490a7f779089dce0f42
Kurt Santos <kurtsantos924@gmail.com>
2025-03-05 11:36:05 +0800

    Initialized Blog app and set up model fields

---
+++ hobbysite/blog/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+# Create your models here.
+class ArticleCategory(models.Model):
+	name = models.CharField(max_length=255, null=True)
+	description = models.TextField()
+
+	class Meta:
+		ordering = ['name']
+
+class Article(models.Model):
+	title = models.CharField(max_length=255)
+	category = models.ForeignKey(ArticleCategory, on_delete=models.SET_NULL, null=True)
+	entry = models.TextField()
+	created_on = models.DateTimeField(auto_now_add=True)
+	updated_on = models.DateTimeField(auto_now=True)
+
+	class Meta:
+		ordering = ['-created_on']
