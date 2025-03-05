107fe2e65afe47464f19d7fb732c8ae44da54aac
Kurt Santos <kurtsantos924@gmail.com>
2025-03-05 12:32:46 +0800

    Formatted files to follow Pep8 standards

--- hobbysite/blog/urls.py
+++ hobbysite/blog/urls.py
@@ -3,7 +3,11 @@ from .views import ArticleListView, ArticleDetailView
 
 urlpatterns = [
     path('articles/', ArticleListView.as_view(), name='article_list'),
-    path('article/<int:pk>/', ArticleDetailView.as_view(), name='article_detail'),
+    path(
+        'article/<int:pk>/',
+        ArticleDetailView.as_view(),
+        name='article_detail'
+    ),
 ]
 
 app_name = "blog"
