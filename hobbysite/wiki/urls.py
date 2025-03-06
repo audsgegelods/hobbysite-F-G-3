6b3c99abe32944e437e18cf50cd2c4425cd107cf
ShaiDayag <shaan.dayag@student.ateneo.edu>
2025-03-07 06:45:11 +0800

    Changed app_name from single-quote strings to double, removed unnecessary comments from the templates that indicated their path, created the models for Article and ArticleCategory

--- hobbysite/wiki/urls.py
+++ hobbysite/wiki/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from .views import ArticleListView, ArticleDetailView
 
-app_name = 'wiki'
+app_name = "wiki"
 urlpatterns = [
     path('articles/', ArticleListView.as_view(), name='article-list'),
     path('article/<int:pk>/', ArticleDetailView.as_view(), name='article-detail'),
