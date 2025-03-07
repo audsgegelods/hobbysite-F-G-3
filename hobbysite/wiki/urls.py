363fc5e05c1cad62810c105a1aa1b0c225e2f93f
ShaiDayag <shaan.dayag@student.ateneo.edu>
2025-03-07 22:07:12 +0800

    Modified and cleaned up code to follow PEP8 style conventions using pycodestyle

--- hobbysite/wiki/urls.py
+++ hobbysite/wiki/urls.py
@@ -5,5 +5,9 @@ from .views import ArticleListView, ArticleDetailView
 app_name = "wiki"
 urlpatterns = [
     path('articles/', ArticleListView.as_view(), name='article-list'),
-    path('article/<int:pk>/', ArticleDetailView.as_view(), name='article-detail'),
+    path(
+        'article/<int:pk>/',
+        ArticleDetailView.as_view(),
+        name='article-detail'
+    ),
 ]
