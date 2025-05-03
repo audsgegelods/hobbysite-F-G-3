a96bc1cdf0b5e645476ac06fa9c85ba47e512990
auby <joaquin.tirona@student.ateneo.edu>
2025-05-04 00:36:36 +0800

    Created login page and added redirect URLs in settings.py

--- hobbysite/hobbysite/settings.py
+++ hobbysite/hobbysite/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'user_management',
 ]
 
 MIDDLEWARE = [
@@ -118,6 +119,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+LOGIN_REDIRECT_URL = ''
+LOGOUT_REDIRECT_URL = '/accounts/login'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
