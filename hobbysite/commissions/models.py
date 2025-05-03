f269dd4c7e4729728f6c0482cd0691d604bfe03a
auby <joaquin.tirona@student.ateneo.edu>
2025-05-03 19:57:42 +0800

    Updated models, Comments model is now Job, created JobApplication model

--- hobbysite/commissions/models.py
+++ hobbysite/commissions/models.py
@@ -5,7 +5,11 @@ from django.urls import reverse
 class Commission(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
-
+    #TODO: Author
+    status = models.CharField(max_length=None, choices={"Open",
+                                                        "Full",
+                                                        "Completed",
+                                                        "Discontinued"})
     people_required = models.IntegerField()
 
     created_on = models.DateTimeField(auto_now_add=True)
@@ -24,16 +28,33 @@ class Commission(models.Model):
         verbose_name_plural = 'commissions'
 
 
-class Comment(models.Model):
+class Job(models.Model):
     commission = models.ForeignKey(Commission,
                                    on_delete=models.CASCADE,
                                    related_name='commission')
+    role = models.CharField(max_length=255)
+    manpower_required = models.IntegerField()
+    status = models.CharField(max_length=None, choices={"Open", "Full"})
     entry = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
     updated_on = models.DateTimeField(auto_now=True)
 
     class Meta:
-        ordering = ['-created_on']
+        ordering = ['status', '-manpower_required', 'role']
 
         verbose_name = 'comment'
         verbose_name_plural = 'comments'
+
+
+class JobApplication(models.Model):
+    job = models.ForeignKey(Job,
+                            on_delete=models.CASCADE,
+                            related_name='job')
+    #TODO: Applicant
+    status = models.CharField(max_length=None, choices={"Pending", 
+                                                        "Accepted", 
+                                                        "Rejected"})
+    applied_on = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['status', 'applied_on']
