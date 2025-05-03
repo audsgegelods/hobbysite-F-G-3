15b9aa3616819660cda76e622d8810bb58f86392
auby <joaquin.tirona@student.ateneo.edu>
2025-05-04 00:30:52 +0800

    Initial setup of Profile model and Update view

--- hobbysite/user_management/views.py
+++ hobbysite/user_management/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import Profile
+from django.views.generic.edit import UpdateView
 
-# Create your views here.
+
+class ProfileUpdateView(UpdateView):
+    model = Profile
+    template_name = 'profile.html'
+
