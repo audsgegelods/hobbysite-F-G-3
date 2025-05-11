c5273178897a961c512d0eccf14332655fdb3163
SantosKurtMatthew <kurtsantos924@gmail.com>
2025-05-11 15:46:22 +0800

    Edited article list to show articles authored by the user first, separate from other articles. Edited article detail to show the links to other works of the author of the current article.

--- hobbysite/blog/views.py
+++ hobbysite/blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView, UpdateView
@@ -17,8 +18,15 @@ class ArticleDetailView(DetailView):
     model = Article
     template_name = 'article_detail.html'
 
+    #https://stackoverflow.com/questions/31201124/django-detailview-get-all-objects
+    def get_context_data(self, **kwargs):
+        context = super(ArticleDetailView, self).get_context_data(**kwargs)
+        context['all_objects'] = Article.objects.all()
 
-class ArticleCreateView(CreateView):#LoginRequiredMixin, CreateView):
+        return context
+
+
+class ArticleCreateView(LoginRequiredMixin, CreateView):
     model = Article
     fields = '__all__'
     template_name = 'article_form.html'
