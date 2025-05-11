96f5c69c375678b4d62f2e50c73d9b0014400799
Miggy Go <miguelivann1129@gmail.com>
2025-05-12 01:27:09 +0800

    Created additional fields in Product model for stock and ownership, created Transaction model with fields for purchase details

--- hobbysite/merchstore/models.py
+++ hobbysite/merchstore/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.urls import reverse
+from user_management.models import Profile
 
 
 # Create your models here.
@@ -22,8 +23,16 @@ class Product(models.Model):
             null=True,
             related_name="product"
             )
+    owner = models.ForeignKey(
+            Profile,
+            on_delete=models.CASCADE,
+            null=True,
+            related_name="profile"
+            )
     description = models.TextField()
     price = models.DecimalField(max_digits=999999999, decimal_places=2)
+    stock = models.IntegerField()
+    status = models.CharField(max_length=255, choices={"Out of Stock", "On Sale", "Available"})
 
     def __str__(self):
         return self.name
@@ -36,3 +45,20 @@ class Product(models.Model):
 
     def get_absolute_url(self):
         return reverse('merchstore:itempage', args=[self.pk])
+    
+class Transaction(models.Model):
+    buyer = models.ForeignKey(
+        Profile,
+        on_delete=models.CASCADE,
+        null=True,
+        related_name="profile"
+        )
+    product = models.ForeignKey(
+        Product,
+        on_delete=models.CASCADE,
+        null=True,
+        related_name="product"
+        )
+    amount = models.IntegerField()
+    status = models.CharField(max_length=255, choices={"On cart", "To Pay", "To Ship", "To Recieve", "Delivered"})
+    created_on = models.DateTimeField(auto_now_add=True)
