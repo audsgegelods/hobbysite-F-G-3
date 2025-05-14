72c4c41e40cc9da16b54e63a10a5adeb8aecb3aa
ShaiDayag <shaan.dayag@student.ateneo.edu>
2025-05-14 20:24:55 +0800

    Modified view functions

--- hobbysite/wiki/views.py
+++ hobbysite/wiki/views.py
@@ -13,6 +13,7 @@ class ArticleListView(ListView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
+        context['categories'] = ArticleCategory.objects.all()
         context['form'] = ArticleForm()
         return context
 
