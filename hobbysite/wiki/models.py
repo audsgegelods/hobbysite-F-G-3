4c987f2043fe4c387d1f62389867a64b2a71d484
ShaiDayag <shaan.dayag@student.ateneo.edu>
2025-05-14 20:55:32 +0800

    Changed header_image field null to True to avoid errors/warnings

--- hobbysite/wiki/models.py
+++ hobbysite/wiki/models.py
@@ -34,7 +34,7 @@ class Article(models.Model):
         related_name='articles'
     )
     entry = models.TextField()
-    header_image = models.ImageField(upload_to='images/', null=False)
+    header_image = models.ImageField(upload_to='images/', null=True)
     created_on = models.DateTimeField(auto_now_add=True, editable=False)
     updated_on = models.DateTimeField(auto_now=True)
 
