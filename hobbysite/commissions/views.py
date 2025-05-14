5bb920046cf0971d0b7dd1e23ba0704cfe2e2c72
auby <joaquin.tirona@student.ateneo.edu>
2025-05-14 17:09:43 +0800

    Sorted Commission list byorder indicated in specs, created categories for commissions created by user and applied to by user

--- hobbysite/commissions/views.py
+++ hobbysite/commissions/views.py
@@ -1,5 +1,6 @@
 from .models import Commission, Job, JobApplication
 from .forms import JobAppForm
+from django.db.models import  Case, When
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import UpdateView, CreateView
@@ -10,6 +11,45 @@ from django.urls import reverse
 class ComListView(ListView):
     model = Commission
     template_name = 'commission-list.html'
+
+    def annotate_queryset(self):
+        comms = Commission.objects.annotate(
+            ordering = Case(
+                When(status='Open', then=0),
+                When(status='Full', then=1),
+                When(status='Completed', then=2),
+                When(status='Discontinued', then=3)
+            )
+        ).order_by('ordering')
+        return comms        
+    
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        
+        sorted_comms = Commission.objects.annotate(
+            ordering = Case(
+                When(status='Open', then=0),
+                When(status='Full', then=1),
+                When(status='Completed', then=2),
+                When(status='Discontinued', then=3)
+            )
+        ).order_by('ordering')
+
+        context['created_comms'] = []
+        for comm in sorted_comms:
+            if comm.author == self.request.user:
+                context['created_comms'].append(comm)
+
+        context['applied_comms'] = [] #TODO TEMP
+        for jobApp in JobApplication.objects.all():
+            if jobApp.applicant == self.request.user:
+                context['applied_comms'].append(jobApp.job.commission)
+
+        context['sorted_comms'] = sorted_comms
+        return context
+    
 
 
 class ComDetailView(DetailView):
@@ -28,13 +68,14 @@ class ComDetailView(DetailView):
         context['open_manpower'] = 0 #TODO Temp
         context['form'] = JobAppForm()
         context['job'] = Job()
+        context['jobApp_list'] = JobApplication.objects.all()
         return context
 
     def post(self, request, *args, **kwargs):
         form = JobAppForm(request.POST)
         if form.is_valid():
             form.instance.applicant = self.request.user
-            #form.instance.commission = self.get_object()
+            form.instance.job = self.get_object()
 
             form.save()
             return self.get(request, *args, **kwargs)
