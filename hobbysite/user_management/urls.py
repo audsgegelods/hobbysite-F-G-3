2e26c9da0e8eca02edf6f320c5b53f18d9f57979
ShaiDayag <shaan.dayag@student.ateneo.edu>
2025-05-14 20:53:00 +0800

    Removed a slash to prevent warnings/errors

--- hobbysite/user_management/urls.py
+++ hobbysite/user_management/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import ProfileUpdateView
 
 urlpatterns = [
-    path('/profile/<str:name>', ProfileUpdateView.as_view(), name='profile'),
+    path('profile/<str:name>', ProfileUpdateView.as_view(), name='profile'),
 ]
