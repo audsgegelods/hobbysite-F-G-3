a6267975339feaa8d5bb5e065ba69c3784d012c2
ShaiDayag <shaan.dayag@student.ateneo.edu>
2025-05-14 20:30:04 +0800

    Created a CommentAdmin for the new Comment model, an ArticleInline to be used by the ArticleCategoryAdmin, and a CommentInline to be used by the ArticleAdmin, as well as modified displays, filters, search fields, and fields for ease of use

--- hobbysite/wiki/admin.py
+++ hobbysite/wiki/admin.py
@@ -1,15 +1,46 @@
 from django.contrib import admin
 
-from .models import ArticleCategory, Article
+from .models import ArticleCategory, Article, Comment
+
+
+class ArticleInline(admin.TabularInline):
+    model = Article
+
+class CommentInline(admin.StackedInline):
+    model = Comment
 
 
 class ArticleCategoryAdmin(admin.ModelAdmin):
     model = ArticleCategory
+    inlines = [ArticleInline, ]
+
+    list_display = ['name', 'description']
+    search_fields = ['name']
+
 
 
 class ArticleAdmin(admin.ModelAdmin):
     model = Article
+    inlines = [CommentInline, ]
+
+    list_display = ['header_image', 'title', 'author__name', 'category__name', 'created_on', 'updated_on']
+    search_fields = ['title', 'author__name', 'category__name']
+    list_filter = ['title', 'author__name', 'updated_on']
+
+    readonly_fields = ['author', 'created_on']
+
+
+class CommentAdmin(admin.ModelAdmin):
+    model = Comment
+
+    list_display = ['article__header_image', 'article__title', 'author__name', 'created_on', 'updated_on']
+    search_fields = ['article__title', 'author__name']
+    list_filter = ['author__name', 'updated_on']
+
+    fields = ['author', 'article', 'entry', 'created_on', 'updated_on']
+    readonly_fields = ['author', 'created_on']
 
 
 admin.site.register(ArticleCategory, ArticleCategoryAdmin)
 admin.site.register(Article, ArticleAdmin)
+admin.site.register(Comment, CommentAdmin)
