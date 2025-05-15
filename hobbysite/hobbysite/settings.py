4c9c26f0ba499b3715c0ab3a7fe385c52d789713
ShaiDayag <shaan.dayag@student.ateneo.edu>
2025-05-15 22:29:38 +0800

    Added the app url under relevant project-level files

--- hobbysite/hobbysite/settings.py
+++ hobbysite/hobbysite/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'user_management',
+    'profile',
 ]
 
 MIDDLEWARE = [
@@ -120,7 +120,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = [os.path.join(BASE_DIR, 'static')]
-LOGIN_REDIRECT_URL = '/blog/articles'
+# LOGIN_REDIRECT_URL = '/blog/articles'
 LOGOUT_REDIRECT_URL = '/accounts/login'
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
