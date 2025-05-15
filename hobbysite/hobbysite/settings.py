fa543a9901480e4d0bc3c8d639bb449388aaeb93
SantosKurtMatthew <kurtsantos924@gmail.com>
2025-05-15 22:52:34 +0800

    edited settings.py

--- hobbysite/hobbysite/settings.py
+++ hobbysite/hobbysite/settings.py
@@ -128,8 +128,8 @@ MEDIA_ROOT = BASE_DIR/'media'
 MEDIA_URL = '/media/'
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = [BASE_DIR/'static']
 
 from .cdn.conf import * 
 
