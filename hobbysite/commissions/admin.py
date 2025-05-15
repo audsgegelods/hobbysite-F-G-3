76c55af4cb1c7ba2a7af0defa13bad1ba5e07f36
ShaiDayag <shaan.dayag@student.ateneo.edu>
2025-05-16 00:40:12 +0800

    Corrected the use of User model with the use of Profile instead, along with the corrected imports

--- hobbysite/commissions/admin.py
+++ hobbysite/commissions/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from .models import Commission, Job, JobApplication 
-from user_management.models import Profile
+from profile.models import Profile
 
 
 class ProfileInline(admin.TabularInline):
