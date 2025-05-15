10b1a2024e0e736abb9cbaf77072767c2fbc24ee
ShaiDayag <shaan.dayag@student.ateneo.edu>
2025-05-15 15:50:50 +0800

    Modified admin panels to correct errors and cleaned up code for PEP8

--- hobbysite/wiki/admin.py
+++ hobbysite/wiki/admin.py
@@ -6,6 +6,7 @@ from .models import ArticleCategory, Article, Comment
 class ArticleInline(admin.TabularInline):
     model = Article
 
+
 class CommentInline(admin.StackedInline):
     model = Comment
 
@@ -18,14 +19,23 @@ class ArticleCategoryAdmin(admin.ModelAdmin):
     search_fields = ['name']
 
 
-
 class ArticleAdmin(admin.ModelAdmin):
     model = Article
     inlines = [CommentInline, ]
 
-    list_display = ['title', 'author__name', 'category__name', 'created_on', 'updated_on']
-    search_fields = ['title', 'author__name', 'category__name']
-    list_filter = ['title', 'author__name', 'updated_on']
+    list_display = ['title', 'author', 'category', 'created_on', 'updated_on']
+    search_fields = ['title', 'author', 'category']
+    list_filter = ['title', 'author', 'updated_on']
+
+    fields = [
+        'author',
+        'title',
+        'header_image',
+        'category',
+        'entry',
+        'created_on',
+        'updated_on'
+    ]
 
     readonly_fields = ['author', 'created_on']
 
@@ -33,9 +43,9 @@ class ArticleAdmin(admin.ModelAdmin):
 class CommentAdmin(admin.ModelAdmin):
     model = Comment
 
-    list_display = ['article__title', 'author__name', 'created_on', 'updated_on']
-    search_fields = ['article__title', 'author__name']
-    list_filter = ['author__name', 'updated_on']
+    list_display = ['article__title', 'author', 'created_on', 'updated_on']
+    search_fields = ['article__title', 'author']
+    list_filter = ['author', 'updated_on']
 
     fields = ['author', 'article', 'entry', 'created_on', 'updated_on']
     readonly_fields = ['author', 'created_on']
