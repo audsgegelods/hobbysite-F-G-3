33fef329b5576d91a3d78fd061c4a6cedbf0d81d
SantosKurtMatthew <kurtsantos924@gmail.com>
2025-05-15 22:36:27 +0800

    edited conf.py

--- hobbysite/hobbysite/cdn/conf.py
+++ hobbysite/hobbysite/cdn/conf.py
@@ -2,12 +2,12 @@ import os
 
 AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
 AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
-AWS_STORAGE_BUCKET_NAME="hobbysite-bucket"
+AWS_STORAGE_BUCKET_NAME=os.environ.get("AWS_STORAGE_BUCKET_NAME")
 AWS_S3_ENDPOINT_URL="https://hobbysite-bucket.sgp1.digitaloceanspaces.com"
 AWS_S3_OBJECT_PARAMETERS = {
 	"CacheControl": "max_age=86400"
 }
-AWS_LOCATION = "https://hobbysite-bucket.sgp1.digitaloceanspaces.com"
+AWS_LOCATION = f"https://{AWS_STORAGE_BUCKET_NAME}.sgp1.digitaloceanspaces.com"
 
 STORAGES = {
 	'default' : {
