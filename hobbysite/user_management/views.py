2cfefd51c492df29445f9c6eac89cb3707f8ad24
ShaiDayag <shaan.dayag@student.ateneo.edu>
2025-05-17 02:02:48 +0800

    Removed user_management app

--- hobbysite/user_management/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from .models import Profile
-from django.views.generic.edit import UpdateView
-
-
-class ProfileUpdateView(UpdateView):
-    model = Profile
-    template_name = 'profile.html'
-
