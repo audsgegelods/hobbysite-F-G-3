8cea8c77fa815723c19b09de6b238c7625ceb02c
ShaiDayag <shaan.dayag@student.ateneo.edu>
2025-05-17 00:58:32 +0800

    Re-added initial commits to fix an error

--- hobbysite/forum/migrations/0001_initial.py
+++ hobbysite/forum/migrations/0001_initial.py
@@ -13,7 +13,7 @@ class Migration(migrations.Migration):
 
     operations = [
         migrations.CreateModel(
-            name='ThreadCategory',
+            name='PostCategory',
             fields=[
                 ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('name', models.CharField(max_length=255, null=True)),
@@ -24,14 +24,14 @@ class Migration(migrations.Migration):
             },
         ),
         migrations.CreateModel(
-            name='Thread',
+            name='Post',
             fields=[
                 ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('title', models.CharField(max_length=255)),
                 ('entry', models.TextField()),
                 ('created_on', models.DateTimeField(auto_now_add=True)),
                 ('updated_on', models.DateTimeField(auto_now=True)),
-                ('category', models.ForeignKey(null=True, on_delete=django.db.models.deletion.SET_NULL, to='forum.ThreadCategory')),
+                ('category', models.ForeignKey(null=True, on_delete=django.db.models.deletion.SET_NULL, to='forum.postcategory')),
             ],
             options={
                 'ordering': ['-created_on'],
