ddac42c9c49fa393427ab50a21ba9e41c7b63930
ShaiDayag <shaan.dayag@student.ateneo.edu>
2025-05-17 00:47:22 +0800

    Fixed the name of the models

--- hobbysite/forum/migrations/0001_initial.py
+++ hobbysite/forum/migrations/0001_initial.py
@@ -13,7 +13,7 @@ class Migration(migrations.Migration):
 
     operations = [
         migrations.CreateModel(
-            name='PostCategory',
+            name='ThreadCategory',
             fields=[
                 ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('name', models.CharField(max_length=255, null=True)),
@@ -24,14 +24,14 @@ class Migration(migrations.Migration):
             },
         ),
         migrations.CreateModel(
-            name='Post',
+            name='Thread',
             fields=[
                 ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('title', models.CharField(max_length=255)),
                 ('entry', models.TextField()),
                 ('created_on', models.DateTimeField(auto_now_add=True)),
                 ('updated_on', models.DateTimeField(auto_now=True)),
-                ('category', models.ForeignKey(null=True, on_delete=django.db.models.deletion.SET_NULL, to='forum.postcategory')),
+                ('category', models.ForeignKey(null=True, on_delete=django.db.models.deletion.SET_NULL, to='forum.ThreadCategory')),
             ],
             options={
                 'ordering': ['-created_on'],
