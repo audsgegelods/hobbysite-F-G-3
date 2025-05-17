cf7634138ef49c918382b8144828726d0251cc9d
ShaiDayag <shaan.dayag@student.ateneo.edu>
2025-05-17 10:21:10 +0800

    Re-added an import that was accidentally removed

--- hobbysite/hobbysite/urls.py
+++ hobbysite/hobbysite/urls.py
@@ -19,6 +19,8 @@ from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
 
+from .views import homepage
+
 urlpatterns = [
     path('', homepage, name='homepage'),
     path('admin/', admin.site.urls),
