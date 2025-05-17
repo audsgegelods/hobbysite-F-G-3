5686527a696f0e5688b92b9a523ffa9ceb1a63dd
auby <joaquin.tirona@student.ateneo.edu>
2025-05-17 15:09:58 +0800

    Created new view for profile creation and created a UserCreateForm

--- hobbysite/profile/forms.py
+++ hobbysite/profile/forms.py
@@ -1,4 +1,6 @@
 from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 from .models import Profile
 
 
@@ -12,3 +14,10 @@ class ProfileForm(forms.ModelForm):
             'email_address': ('Profile Email')
         }
 
+
+class UserCreateForm(UserCreationForm):
+    display_name = forms.CharField(max_length=63)
+
+    class Meta:
+        model = User
+        fields = ['username', 'password1', 'password2', 'email']
