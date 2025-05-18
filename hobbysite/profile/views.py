293f7773b7144f43c89852b1fa2b0a9cb7063d51
SantosKurtMatthew <kurtsantos924@gmail.com>
2025-05-18 16:26:43 +0800

    Made Profiles automatically made after creating users. Added logout button and register button to homepage. Added "logged in as: " text to header.

--- hobbysite/profile/views.py
+++ hobbysite/profile/views.py
@@ -1,5 +1,7 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic.edit import CreateView, UpdateView
+from django.urls import reverse
+from django.contrib.auth.models import User
 from .models import Profile
 from .forms import ProfileForm, UserCreateForm
 
@@ -15,8 +17,18 @@ class ProfileCreateView(CreateView):
         return context
 
     def form_valid(self, form):
-        form.instance.user = self.request.user
+        form.save()
+        new_profile = Profile(
+            user=User.objects.get(username=form.cleaned_data['username']),
+            display_name=form.cleaned_data['display_name'],
+            email_address=form.cleaned_data['email']
+        )
+        new_profile.save()
+
         return super(ProfileCreateView, self).form_valid(form)
+
+    def get_success_url(self):
+        return reverse('homepage', kwargs={})
 
 
 class ProfileUpdateView(LoginRequiredMixin, UpdateView):
@@ -25,7 +37,11 @@ class ProfileUpdateView(LoginRequiredMixin, UpdateView):
     form_class = ProfileForm
 
     def form_valid(self, form):
-        if self.request.user in Profile.objects.all():
-            form.instance.user = self.request.user
-            form.instance.email_address.widget.attrs['readonly'] = True
-            return super(ProfileUpdateView, self).form_valid(form)
+        #if self.request.user in Profile.objects.all():
+        #    form.instance.user = self.request.user
+        #   form.instance.email_address.widget.attrs['readonly'] = True
+        return super(ProfileUpdateView, self).form_valid(form)
+
+    def get_success_url(self):
+        return reverse('profile:profile', args=[self.get_object().pk])
+        #return super(ProfileUpdateView, self).form_valid(form)
